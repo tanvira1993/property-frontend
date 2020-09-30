1ce55a164158fb6ab2063bd8ef8bd227c4eb871a
tanvira <tanvira1993@gmail.com>
2020-09-30 14:42:28 +0600

    routing added

--- routes.js
+++ routes.js
@@ -62,4 +62,58 @@ propertyApp.config(['$stateProvider', '$urlRouterProvider','$locationProvider', 
         }
     })
 
+    .state('property', {
+        url: "/property",
+        templateUrl: "layouts/property.html",
+        data: {pageTitle: 'Property'},
+        controller: "PropertyController",
+        resolve: {
+            deps: ['$ocLazyLoad', function ($ocLazyLoad) {
+                return $ocLazyLoad.load({
+                    name: 'propertyApp',
+                    insertBefore: '#ng_load_plugins_before', // load the above css files before a LINK element with this ID. Dynamic CSS files must be loaded between core and theme css files
+                    files: [
+                    'ng-asset/PropertyController.js'
+                    ]
+                });
+            }]
+        }
+    })
+
+    .state('details', {
+        url: "/propertyDetails",
+        templateUrl: "layouts/propertyDetails.html",
+        data: {pageTitle: 'Property Details'},
+        controller: "PropertyDetailsController",
+        resolve: {
+            deps: ['$ocLazyLoad', function ($ocLazyLoad) {
+                return $ocLazyLoad.load({
+                    name: 'propertyApp',
+                    insertBefore: '#ng_load_plugins_before', // load the above css files before a LINK element with this ID. Dynamic CSS files must be loaded between core and theme css files
+                    files: [
+                    'ng-asset/PropertyDetailsController.js'
+                    ]
+                });
+            }]
+        }
+    })
+
+    .state('sell', {
+        url: "/submitProperty",
+        templateUrl: "layouts/sellProperty.html",
+        data: {pageTitle: 'Sell Property '},
+        controller: "PropertySellController",
+        resolve: {
+            deps: ['$ocLazyLoad', function ($ocLazyLoad) {
+                return $ocLazyLoad.load({
+                    name: 'propertyApp',
+                    insertBefore: '#ng_load_plugins_before', // load the above css files before a LINK element with this ID. Dynamic CSS files must be loaded between core and theme css files
+                    files: [
+                    'ng-asset/PropertySellController.js'
+                    ]
+                });
+            }]
+        }
+    })
+
 }]);
